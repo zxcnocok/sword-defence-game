a6db057068b143bc9e56c359fec06d6518a46400
Artttyom7 <artyomvoronko@yandex.by>
2025-12-08 13:17:31 +0300

    Add files via upload

---
+++ игра-2/menu.gd
@@ -0,0 +1,14 @@
+extends CanvasLayer
+
+
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://node_2d.tscn")
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_setting_pressed() -> void:
+	get_tree().change_scene_to_file("res://options.tscn")
